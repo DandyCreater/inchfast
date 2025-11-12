3f2f7c67785c82f9bad6302dd362f9e15b0a8e23
DandyCreater <dandyvialpando@gmail.com>
2025-11-12 23:34:18 +0700

    Fix index.html for mobile stability

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"a18df97ca57a249df5d8d68cd0820600223ce2
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "3250304789"
+    serviceWorkerVersion: "2445503239"
   }
 });
